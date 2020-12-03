Fixed the gripper rotation angle error by converting all angles to -pi to pi by plus/minus 2*pi.

Robot_sim.py
@@ -504,14 +504,27 @@ class Robot(object):
     def _rotate_gripper_z(self, target_angle, num_step=None):
         _, current_angle = vrep.simxGetObjectOrientation(self.sim_client, self.UR5_target_handle, -1,
                                                          vrep.simx_opmode_blocking)
+        # comments:
         # if target_angle > np.pi:
         #     target_angle -= np.pi
         # if target_angle < -np.pi:
         #     target_angle += np.pi
-        if target_angle > np.pi + 0.2:
+
+        # original:
+        # if target_angle > np.pi + 0.2:
+        #     target_angle -= 2*np.pi
+        # if target_angle < -np.pi - 0.2:
+        #     target_angle += 2*np.pi
+
+        if current_angle[2] > np.pi:
+            current_angle[2] -= 2*np.pi
+        if current_angle[2] < -np.pi:
+            current_angle[2] += 2*np.pi
+        while target_angle > np.pi:
             target_angle -= 2*np.pi
-        if target_angle < -np.pi - 0.2:
+        while target_angle < -np.pi:
             target_angle += 2*np.pi
+        
         ori_direction = np.asarray([0.,
                                     0.,
                                     target_angle - current_angle[2]])

main.py
@@ -32,13 +32,13 @@ def main(args):
     num_obj = args.num_obj if is_sim else None
     # IP and port to robot arm as TCP client (UR5)
     tcp_host_ip = args.tcp_host_ip if not is_sim else None
-    tcp_port = args.tcp_port if not is_s-im else None
+    tcp_port = args.tcp_port if not is_sim else None
     # IP and port to robot arm as real-time client (UR5)
     rtc_host_ip = args.rtc_host_ip if not is_sim else None
     rtc_port = args.rtc_port if not is_sim else None
     if is_sim:
         # Cols: min max, Rows: x y z (workspace limits in robot coordinates)
-        args.workspace_limits = np.asarray([[0.2,, 0.7], [-0.25, 0.25], [0.0002, 0.2]])
+        args.workspace_limits = np.asarray([[0.2, 0.7], [-0.25, 0.25], [0.0002, 0.2]])
         '''
         np.asarray([[-0.724, -0.276], [-0.224, 0.224], [-0.0001, 0.4]])
         '''